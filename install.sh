e93c368da51c99966e92e68465bc70f88638b99b
Ilias Chatzopoulos <ilias_xatz@windowslive.com>
2024-02-28 00:52:18 +0200

    remove previous alias

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 current_dir=`pwd`
 command="python3 ${current_dir}/ai-chat-cli.py"
-echo $command
+# Remove old alias
+sed -i '/^alias ai=/d' ~/.bashrc
+# Create new alias
 echo "alias ai='${command}'" >> ~/.bashrc
 echo "Installation Completed"
