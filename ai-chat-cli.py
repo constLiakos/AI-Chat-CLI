e65cff6cc64cf6d24c5d18d4f7b77e74b4dc3d65
Ilias Chatzopoulos <ilias_xatz@windowslive.com>
2024-02-28 03:05:29 +0200

    change to json format for more versatile aproach in prompts using shortcuts

--- ai-chat-cli.py
+++ ai-chat-cli.py
@@ -5,6 +5,7 @@ import re
 from datetime import  date, datetime
 import subprocess
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -12,7 +13,7 @@ LLM_MODEL_NAME = os.getenv('LLM_MODEL')
 USER_NAME = os.getenv('USER_NAME')
 AI_NAME = os.getenv('AI_NAME')
 PRINTED_REPLY_HEAD = os.getenv('REPLY_HEAD')
-LLM_PROMPT_FILENANME = os.getenv('PROMPT_FILENANME')
+CONFIG_FILENANME = os.getenv('CONFIG_FILENANME')
 LLM_API_BASE = os.getenv('API_BASE')
 LLM_API_KEY= os.getenv('API_KEY')
 LLM_TEMPERATURE = float(os.getenv('LLM_TEMPERATURE'))
@@ -31,19 +32,64 @@ knowledge_str = "Knowledge: " + '\n'.join(KNOWLEDGE)
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-file_path = os.path.join(dir_path, LLM_PROMPT_FILENANME)
+file_path = os.path.join(dir_path, CONFIG_FILENANME)
 f = open(file_path, "r")
-
-SYSTEM_PROMPT = f.read() + knowledge_str
+config_raw_data = f.read()
 f.close()
+
+class Prompt():
+    def __init__(self, name, prompt, info,  description=None, shortcuts=None ):
+        self.name = name
+        self.prompt = prompt
+        self.info = info
+        self.description = description
+        self.shortcuts = shortcuts if shortcuts else []
+        
+
+class Config:
+    def __init__(self, prompts_data, save):
+        # self.prompts = [Prompt(**prompt) for prompt in prompts_data]
+        self.prompts = {prompt['name']: Prompt(**prompt) for prompt in prompts_data}
+        self.save = save
+        self.shortcuts_to_prompt = self._build_shortcuts_dict()
+
+    def _build_shortcuts_dict(self):
+        shortcuts_dict = {}
+        for prompt in self.prompts.values():
+            for shortcut in prompt.shortcuts:
+                shortcuts_dict[shortcut] = prompt
+        return shortcuts_dict
+
+parsed_json = json.loads(config_raw_data)
+
+config = Config(parsed_json['prompts'], parsed_json['save'])
+
+SYSTEM_PROMPT = config.prompts['default'].prompt
 
 user_input = ""
 
+def custom_prompt_resolver(test_if_shortcut):
+    if test_if_shortcut in config.shortcuts_to_prompt:
+            return config.prompts[test_if_shortcut].prompt
+    return None
+
+
+SYSTEM_PROMPT = config.prompts["default"].prompt
 if len(argv) > 1:
-    for i in range(1,len(argv)):
-        user_input = user_input + " " + argv[i]
+    is_default = True
+    custom_prompt = custom_prompt_resolver(argv[1])
+    if custom_prompt:
+        SYSTEM_PROMPT = custom_prompt
+        is_default = False
     if user_input.lower() == "quit" or user_input.lower() == "exit":
         exit(0)
+
+    user_input_start = 1
+    if is_default != True:
+        user_input_start = 2
+
+    for i in range(user_input_start, len(argv)):
+        user_input = user_input + " " + argv[i]
 else:
     user_input = input(f"{USER_NAME}: ")
 
@@ -51,6 +97,7 @@ messages =  [
                 { "content": SYSTEM_PROMPT ,"role": "system"},
                 { "content": user_input ,"role": "user"},
             ]
+
 def execute_command(response, command):
     if re.search( "#\s*ASK_USER"  , response.response_uptil_now.strip()):
         print("Execute Command: " + command + "\n")
